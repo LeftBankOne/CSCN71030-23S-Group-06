6b4c4c43d25749c0f9d32527f200a76a67afbd41
LeftBankOne <122998285+LeftBankOne@users.noreply.github.com>
2023-08-03 22:36:01 -0400

    Update Main.c
    Added openFile Tests

--- CSCN71030-23S-Group-06/Main.c
+++ CSCN71030-23S-Group-06/Main.c
@@ -4,88 +4,32 @@
 #include "Function.h"
 
 int main() {
-    int choice;
-    int numRecipesToAdd;
-    int recipeToDelete;
+  
+    // Test Case 1: Valid File and Mode
+    const char* filename1 = "testfile.txt";
+    const char* mode1 = "w"; 
+    FILE* file1 = openFile(filename1, mode1);
+    if (file1 != NULL) 
+    {
+        printf("Test Case 1: File created successfully\n");
+        fclose(file1);
+    }
+    else 
+    {
+        printf("Test Case 1: Error creating file\n");
+    }
+    // Should run
 
-    // Get the initial number of recipes from the file
-    int numRecipes = getNumRecipes("Recipes.txt"); // Get the number of recipes from "Recipes.txt"
+    //// Test Case 2: Invalid File
+    //const char* filename2 = "non_existent_file.txt";
+    //const char* mode2 = "r";
+    //FILE* file2 = openFile(filename2, mode2);
+    //// Should return an error when trying to open the file
 
-    do {
-        displayMenu();
-        int scanfResult = scanf("%d", &choice);
-        int c;
-        while ((c = getchar()) != '\n' && c != EOF); // Consume any remaining characters in the input buffer
-        if (scanfResult != 1) {
-            printf("Invalid input. Please enter a number.\n");
-        }
-        else {
-
-            switch (choice) {
-            case 1:
-                // Show the number of recipes in the file and list all the recipes
-                showRecipesFromFile("Recipes.txt");
-                break;
-            case 2:
-                printf("Enter the number of additional recipes to add: ");
-                scanf("%d", &numRecipesToAdd);
-                getchar(); // Consume the newline character left by scanf
-                // Open or create the file if it's not existed
-                FILE* file = openFile("Recipes.txt", "a"); // Open file name "Recipes.txt" for appending
-
-                for (int i = 0; i < numRecipesToAdd; i++) {
-                    char name[100];
-                    char ingredients[500];
-                    char instructions[1000];
-
-                    printf("Enter the name of Recipe %d: ", i + 1);
-                    fgets(name, sizeof(name), stdin);
-                    name[strcspn(name, "\n")] = '\0';
-
-                    printf("Enter the ingredients of Recipe %d: ", i + 1);
-                    fgets(ingredients, sizeof(ingredients), stdin);
-                    ingredients[strcspn(ingredients, "\n")] = '\0';
-
-                    printf("Enter the instructions of Recipe %d: ", i + 1);
-                    fgets(instructions, sizeof(instructions), stdin);
-                    instructions[strcspn(instructions, "\n")] = '\0';
-
-                    addRecipe(name, ingredients, instructions);
-                    // Write the data to file
-                    writeRecipeToFile(file, &recipeDatabase[i]);
-                }
-                fclose(file);
-                break;
-            case 3:
-                // Delete a recipe
-                printf("Enter the recipe number to delete: ");
-                int scanfResult = scanf("%d", &recipeToDelete);
-                while ((c = getchar()) != '\n' && c != EOF); 
-
-                if (scanfResult != 1) {
-                    printf("Invalid input. Please enter a number.\n");
-                }
-                else {
-                    if (recipeToDelete < 1 || recipeToDelete > numRecipes) {
-                        printf("Invalid recipe number. Please enter a valid recipe number.\n");
-                    }
-                    else {
-                        deleteRecipeFromFile("Recipes.txt", recipeToDelete);
-                        numRecipes--;
-                    }
-                }
-            case 4:
-                // Generate and display a random recipe
-                generateRecipe("Recipes.txt");
-                break;
-
-            case 5:
-                printf("Exiting the program.\n");
-                break;
-            default:
-                printf("Invalid choice. Please try again.\n");
-            }
-        }
-    } while (choice != 5);
+    //// Test Case 3: Invalid Mode
+    //const char* filename3 = "testfile.txt";
+    //const char* mode3 = "invalid_mode";
+    //FILE* file3 = openFile(filename3, mode3);
+    //// Should return an error when using the wrong mode
     return 0;
 }
